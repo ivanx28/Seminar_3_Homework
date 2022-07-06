2e4c9dc6a34d40f6e8631bdf8c2dae5deb3736c1
ivan <lopatin.ivan28@icloud.com>
2022-07-06 21:04:32 +0300

    add task 19. Simple version

---
+++ task_19_simple/Program.cs
@@ -0,0 +1,27 @@
+﻿// Программа которая определяет является ли введённое пятизначное число палиндромом.
+// простой способ, работающий только для пятизначного числа.
+
+string checkPalindrom(int number)
+{
+    int num1 = number / 10000;
+    int num2 = (number % 10000) / 1000;
+    int num4 = (number % 100) / 10;
+    int num5 = number % 10;
+    
+    if (num1 == num5 && num2 == num4)
+        return "Введённое число является палиндромом";
+    else
+        return "Введённое числое не является палиндромом";
+}
+
+Console.WriteLine("Введите любое пятизначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 10000 && num <= 99999)
+    {
+    Console.WriteLine (checkPalindrom(num));
+    }
+else 
+    {
+    Console.WriteLine ("Вы ввели не пятизначное число");
+    }
