6d0e225e3731823f9f944831585e5c57a42bccfe
ivan <lopatin.ivan28@icloud.com>
2022-07-06 21:05:15 +0300

    add task 23

---
+++ task_23/Program.cs
@@ -0,0 +1,19 @@
+﻿// Программа, показывающая таблицу кубов от 1 до введённого числа N (включительно).
+
+void cube (int number)
+{
+    double x = 1;
+    int i = 1;
+    while (i <= number)
+        {
+        x = Math.Pow(i, 3);
+        Console.Write(x + "|");
+        i++;
+        }
+}
+
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+
+cube(N);
+Console.WriteLine();
