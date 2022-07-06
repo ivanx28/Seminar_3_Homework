24a545043af3343891fb773556cd7feccd57afb2
ivan <lopatin.ivan28@icloud.com>
2022-07-06 21:03:45 +0300

    add task 19

---
+++ task_19/Program.cs
@@ -0,0 +1,27 @@
+﻿// Программа которая определяет является ли введённое число палиндромом.
+// Также данная программа работает для числа из любого количества цифр.
+
+
+int doubleNumber (int x)                //Данным метод переворачивает введённое число "задом наперёд".
+{
+    int num1 = 0;
+    int numDouble = 0;
+    while (x > 0)
+    {
+        num1 = x % 10;
+        numDouble = numDouble*10+num1;
+        x = x / 10;
+    }
+    return numDouble;
+}
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int numberDouble = doubleNumber(number);
+
+if (number == numberDouble) Console.WriteLine("Введённое число является палиндромом");
+else Console.WriteLine("Введённое число не является палиндромом");
+
+
+
